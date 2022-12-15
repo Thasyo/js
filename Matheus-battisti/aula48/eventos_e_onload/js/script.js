a023afb35d93e4ea8938b57e149e4fcd50363bcf
Thasyo Peres <thasyotp2002@gmail.com>
2022-12-15 00:11:51 -0300

    atualizando progresso

---
+++ Matheus-battisti/aula48/eventos_e_onload/js/script.js
@@ -0,0 +1,16 @@
+//Onload na prática.
+ window.onload = function(){
+
+    console.log("carregando página...");
+
+ }
+
+ console.log("carregou o JS")
+
+//o onload espera carregar toda a parte de HTML para depois ir pro Javascript.
+
+
+
+
+
+
